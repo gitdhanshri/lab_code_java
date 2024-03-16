63e62e37e3c3a8cae010203471299a0531cc85e2
gitdhanshri <2022bit049@sggs.ac.in>
2024-03-16 21:39:32 +0530

    Adding two files having same .class file

---
+++ sameFileName1.java
@@ -0,0 +1,9 @@
+class sameClassName{
+
+	static{
+		System.out.println("Inside Static block of sameClassName1  class");
+	}
+	public static void main(String args[]){
+		sameClassName c1=new sameClassName();	
+	}
+}
