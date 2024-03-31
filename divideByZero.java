b7c196ee3400f27fd41710ce93ffaf99753c1f01
gitdhanshri <2022bit049@sggs.ac.in>
2024-03-31 21:04:58 +0530

    Dividing by Zero

---
+++ divideByZero.java
@@ -0,0 +1,9 @@
+class ExceptionByZero{
+	public static void main(String args[]){
+		ExceptionByZero E1=new ExceptionByZero();
+		int num=10;
+		System.out.println(num);
+		int num2=num/0;
+		System.out.println(num2);
+	}
+}
