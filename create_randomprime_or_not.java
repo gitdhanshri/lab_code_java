a6820ec95ad23153c0abbbefa8edc23bbd3b18ab
gitdhanshri <2022bit049@sggs.ac.in>
2024-04-02 22:29:04 +0530

    modification

--- create_randomprime_or_not.java
+++ create_randomprime_or_not.java
@@ -1,79 +1,103 @@
-class SY{ 
-//int random_number;
-static int prime_count=0;
-static int non_prime_count=0;
+class prime{
+static int primeCount=0;
+ static int nonPrimeCount=0;
 public static boolean isPrime(long num){
-	if(num<2){
-		return false;
-	}
-	if(num==2||num==3){
-		return true;
-	}
-	if(num%2==0||num%3==0){
-		return false;
-	}
+ 	if(num<2){
+ 		return false;
+ 	}
+  	else if(num==2||num==3){
+ 		return true;
+ 	}
+   	else if(num%2==0||num%3==0){
+ 		return false;
+ 	}
+ 	for(int i=5;i<num;i=i+6){
+ 		if(num%i==0||num%(i+2)==0){
+ 			return false;
+ 		}
+ 		else
+ 		{
+ 			return true;
+ 		}
 	
- 	for(int i=5;i<=num;i=i+6){
-		if(num%i==0&&num%(i+2)==0){
-			return false;
-	}
-	else{
-		return true;
-	}
-}
-     	return false;
-}
-public static long  random_7_digit_num(){
+ 	}
+ 	 return true;
+ }
+ public static long generate_1_digit_number(){
 
-	long number_of_7_digits=(random_1_digit()*1000000)+(100000*random_1_digit())+(10000*random_1_digit())+(1000*random_1_digit())+(100*random_1_digit())+(10*random_1_digit())+(1*random_1_digit());
-//System.out.println(number_of_7_digits);
-        return number_of_7_digits ;
+ 	long n1=System.nanoTime();
+ 	/*System.out.println("inside  function-->"+n1);*/
+ 	long num=(n1%10);
+ 	return num;
+ }
+ 
+public static long generate_7_digit_number(){
+	long res=0;
+	for(int i=0;i<7;i++){
+		res=res*10+(generate_1_digit_number());	
+	}
+	return res;
 }
-//count prime and non-prime numers
-public static void count_prime_and_non_primes(long [] array){
-	
-	for(int i=0;i<array.length;i++){
-		if(isPrime(array[i])){
-			prime_count++;
-		}
-	 
-	 else{
-		non_prime_count++;
-	 }
-	 }
-	
-}
-public static long[] store_random_number_in_arary(){
-	long  arr[]=new long[100];
-	for(int i=0;i<50;i++){
-	arr[i]=random_7_digit_num();
+public static long[] generateArrayOf1KAndStoreNumers(){
+	long[] arr= new long[1000000];
+ 
+	for(int i=0;i<1000000;i++){
+		arr[i]=generate_7_digit_number();
 	}
 	return arr;
 }
-public static long random_1_digit(){
-        long num=(System.nanoTime()%10);
-	return num;
-}
-    public static void main(String[] args) 
-    { 
-    	long arr2[]=store_random_number_in_arary();
-    	int i=0;
-    	while(i<5){
-    	 long start_time=System.nanoTime();
-
-    	 count_prime_and_non_primes(arr2);
-    	 
-         long end_time=System.nanoTime();
-         System.out.print("count of prime numbers-->");
-	 System.out.println(prime_count);
-	 System.out.print("count of  non-prime numbers-->");
-         System.out.println(non_prime_count); 
-       
-	System.out.println("time taken "+((end_time)-start_time));
-	prime_count=0;
-	non_prime_count=0;
-	i++;
+public static void  countPrimeNumbersBy_Ifelse(long arr[]){
+	for(int i=0;i<arr.length;i++){
+		if(isPrime(arr[i])==true){
+			primeCount++;
+		}
+		else{
+			 nonPrimeCount++;
+		}
 	}
 }
-}	
+public static void  countPrimeNumbersBy_elseIf(long arr[]){
+	for(int i=0;i<arr.length;i++){
+		if(isPrime(arr[i])!=true){
+			nonPrimeCount++;
+		}
+		else{
+			 primeCount++;
+		}
+	}
+}
+public static void main(String args[]){
 
+long[] arr=generateArrayOf1KAndStoreNumers();
+	/*Approach 1*/
+	int i=0;
+    	while(i<5){
+    	long start_time=System.nanoTime();
+	countPrimeNumbersBy_Ifelse(arr);
+	 long end_time=System.nanoTime();
+	System.out.print("\nPrime count       :  "+primeCount);
+	System.out.print("\nNon-Prime couont  :  "+nonPrimeCount);
+	System.out.print("\nTime Difference   :  "+(end_time-start_time));
+	primeCount=0;
+	nonPrimeCount=0;
+	i++;
+	System.out.print(" \n");
+	}
+	System.out.print("\n \n");
+ 	/*//Approach 2//*/
+	System.out.print(" \nSecond Approach-->");
+	int j=0;
+    	while(j<5){
+        long start_time=System.nanoTime();
+	countPrimeNumbersBy_elseIf(arr);
+	long end_time=System.nanoTime();
+	System.out.print("\nNon-Prime couont  :  "+nonPrimeCount);
+	System.out.print("\nPrime count       :  "+primeCount);
+	System.out.print("\nTime Difference   :  "+(end_time-start_time));
+	primeCount=0;
+	nonPrimeCount=0;
+	j++;
+	System.out.print(" \n");
+	}
+}
+}
